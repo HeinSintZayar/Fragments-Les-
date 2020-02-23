c32157f387b593a5789a86875074ebb118bd40d7
HeinSintZayar <heinsintzayar@gmail.com>
2020-02-23 13:49:44 +0630

    first commit

---
+++ app/src/main/java/com/assassinkiller/fragmentles/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.assassinkiller.fragmentles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
